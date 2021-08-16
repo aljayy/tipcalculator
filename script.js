a9b516a52547888505b5ba8b65cbc5fe8b9d0bfd
Alan Jimenez <stajimenez97@gmai.com>
2021-08-16 14:36:28 -0700

    JavaScript functionality almost complete. Just need to fix NaN error when only inputting num of people

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 
 const billInput = document.getElementById(`info-input-1`);
 const btn = document.querySelectorAll(`.btn-percent`);
+const customPercent = document.querySelector(`#custom-percent`);
 const peopleInput = document.querySelector(`#info-input-2`);
 const tip = document.querySelector(`#tip`);
 const total = document.querySelector(`#total`);
 const error = document.querySelector(`#error`);
+const reset = document.querySelector(`.reset`);
 
 let billValue, percentValue, peopleValue, tipValue;
 
@@ -17,34 +19,72 @@ const calculateAmounts = function () {
     error.style.display = `none`;
     peopleInput.classList.remove(`error-outline`);
     peopleValue = Number(peopleInput.value);
-    tipValue = billValue * (percentValue / peopleValue);
-    tip.textContent = `$${tipValue}`;
-    total.textContent = `$${billValue / peopleValue + tipValue}`;
+    tipValue = (billValue * (percentValue / 100)) / peopleValue;
+    tip.textContent = `$${tipValue.toFixed(2)}`;
+    total.textContent = `$${(billValue / peopleValue + tipValue).toFixed(2)}`;
   }
 };
 
 billInput.addEventListener(`input`, function () {
-  if (percentValue) {
-    console.log(true);
-  }
   billValue = Number(billInput.value);
+
+  if (peopleValue >= 1) calculateAmounts();
 });
 
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener(`click`, function () {
-    if (btn[i].textContent === `5%`) {
-      percentValue = 0.05;
-      console.log(percentValue);
-    } else if (btn[i].textContent === `10%`) {
-      percentValue = 0.1;
-    } else if (btn[i].textContent === `15%`) {
-      percentValue = 0.15;
-    } else if (btn[i].textContent === `25%`) {
-      percentValue = 0.25;
-    } else if (btn[i].textContent === `50%`) {
-      percentValue = 0.5;
+    for (let i = 0; i < btn.length; i++) {
+      btn[i].classList.remove(`btn-focus`);
     }
+    if (btn[i].textContent === `5%`) {
+      percentValue = 5;
+      btn[i].classList.add(`btn-focus`);
+      customPercent.value = ``;
+    } else if (btn[i].textContent === `10%`) {
+      percentValue = 10;
+      btn[i].classList.add(`btn-focus`);
+      customPercent.value = ``;
+    } else if (btn[i].textContent === `15%`) {
+      percentValue = 15;
+      btn[i].classList.add(`btn-focus`);
+      customPercent.value = ``;
+    } else if (btn[i].textContent === `25%`) {
+      percentValue = 25;
+      btn[i].classList.add(`btn-focus`);
+      customPercent.value = ``;
+    } else if (btn[i].textContent === `50%`) {
+      percentValue = 50;
+      btn[i].classList.add(`btn-focus`);
+      customPercent.value = ``;
+    }
+    if (peopleValue >= 1) calculateAmounts();
   });
 }
 
+customPercent.addEventListener(`click`, function () {
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].classList.remove(`btn-focus`);
+  }
+});
+
+customPercent.addEventListener(`input`, function () {
+  percentValue = Number(customPercent.value);
+
+  if (peopleValue >= 1) calculateAmounts();
+});
+
 peopleInput.addEventListener(`input`, calculateAmounts);
+
+reset.addEventListener(`click`, function () {
+  billInput.value = ``;
+  for (let i = 0; i < btn.length; i++) {
+    btn[i].classList.remove(`btn-focus`);
+  }
+  percentValue = ``;
+  customPercent.value = ``;
+  peopleInput.value = ``;
+  tip.textContent = `$0.00`;
+  total.textContent = `$0.00`;
+  error.style.display = `none`;
+  peopleInput.classList.remove(`error-outline`);
+});
